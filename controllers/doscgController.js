02cb6c42f8503969ece867db0a48cacacbcf1616
Thawatchai Kraiwitwattana <thawatchai.krai@gmail.com>
2020-09-04 14:19:54 +0700

    make it better

--- controllers/doscgController.js
+++ controllers/doscgController.js
@@ -27,58 +27,5 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/", async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  ); // If needed
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  const newRecord = new Doscg(req.body);
-  try {
-    const doscg = await newRecord.save();
-    if (!doscg) throw new Error("Error while creating new record.");
-    res.status(200).json(doscg);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// router.post("/", (req, res) => {
-//   const newRecord = new Doscg({
-//     title: `${req.body.title} DOSCG`,
-//   });
-
-//   newRecord.save((err, docs) => {
-//     if (!err) {
-//       res.send(docs);
-//       console.log("Successfully creating new record");
-//     } else {
-//       console.log(
-//         "Error while creating new record :" + JSON.stringify(err, undefined, 2)
-//       );
-//     }
-//   });
-// });
-
-router.delete("/:id", (req, res) => {
-  if (!ObjectID.isValid(req.params.id)) {
-    return res.status(400).send("No record with given id :" + res.params.id);
-  }
-
-  Doscg.findByIdAndDelete(req.params.id, (err, docs) => {
-    if (!err) {
-      res.send(docs);
-      console.log("Successfully delete a record");
-    } else
-      console.log(
-        "Error while delete a record :" + JSON.stringify(err, undefined, 2)
-      );
-  });
-});
 
 module.exports = router;
